0483e94e3abb5cd1c7780723c792258dc261bc52
Hakjoo Kim <ruppi.kim@hardkernel.com>
2013-07-23 17:25:25 +0900

    pinctrl: exynos: add exynos5410 SoC specific data
    Add Samsung EXYNOS5410 SoC specific data to enable pinctrl
    support for all platforms based on EXYNOS5410.

--- drivers/pinctrl/pinctrl-samsung.c
+++ drivers/pinctrl/pinctrl-samsung.c
@@ -1217,6 +1217,8 @@ static const struct of_device_id samsung_pinctrl_dt_match[] = {
 		.data = (void *)exynos4x12_pin_ctrl },
 	{ .compatible = "samsung,exynos5250-pinctrl",
 		.data = (void *)exynos5250_pin_ctrl },
+	{ .compatible = "samsung,exynos5410-pinctrl",
+		.data = (void *)exynos5410_pin_ctrl },
 	{ .compatible = "samsung,exynos5420-pinctrl",
 		.data = (void *)exynos5420_pin_ctrl },
 #endif
